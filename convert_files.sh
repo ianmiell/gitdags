fde0e2bcc2435df6913efcad13bd48402db9556d
Ian Miell <ian.miell@gmail.com>
2018-03-05 13:25:29 +0000

    make

--- convert_files.sh
+++ convert_files.sh
@@ -3,9 +3,14 @@
 set -eux
 
 cd /files
-for f in $(ls *tex)
-do
-	pdflatex ${f}
-	basef=$(echo $f | sed 's/.tex//g')
-	convert -density 600x600 ${basef}.pdf -quality 90 -resize 1080x800 ${basef}.png
-done
+if [ -a Makefile ]
+then
+	make
+else
+	for f in $(ls *tex)
+	do
+		pdflatex ${f}
+		basef=$(echo $f | sed 's/.tex//g')
+		convert -density 600x600 ${basef}.pdf -quality 90 -resize 1080x800 ${basef}.png
+	done
+fi
